261742afada7baaddd22aabe092896ff59eb0d9b
ZahidMahfudz <zaemahfudz@gmail.com>
2023-10-06 17:22:29 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="untukStylecss/style.css">
+    <title>Halaman Home</title>
+</head>
+<body>
+    <p>ini adalah halaman home buat contoh</p>
+    <script src="untukBuatjs/contohalert.js"></script>
+</body>
+</html>
