03d9c462c764640249460f027217f5ea7099b6e4
ZahidMahfudz <zaemahfudz@gmail.com>
2023-10-25 07:11:50 +0700

    coba login

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('login');
+    return view('login.login');
 });
 Route::get('/about', function () {
     return view('about');
